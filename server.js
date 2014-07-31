9dc0ad6b8b9f4aaa3546b0945d5f6f96e76cd478
Paul Cook <paul.cook@adtechzen.com>
2014-07-31 16:51:31 +0100

    heroku deploys to medit8.daddyswork.com

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express'),
     app = express();
-
-var SERVER= { PORT: 8000,
+// PORT is specified on heroku
+var SERVER= { PORT: process.env.PORT || 9000,
 			  maxAge: 0}; //no cache
 
 /**
  * Middleware
  */
-app.use(express.static(__dirname + '/public', { maxAge: SERVER.maxAge }));
+app.use(express.static(__dirname + '/client', { maxAge: SERVER.maxAge }));
 
 console.log ( 'starting on' , SERVER.PORT )
 module.exports = app.listen( SERVER.PORT );
