fa7880fd33892e1b5768a3fc8529a29162557225
Paul Cook <paul.cook@adtechzen.com>
2014-08-25 14:28:34 +0100

    tweaking content color and bottom margin

--- client/scripts/controllers/home-controller.js
+++ client/scripts/controllers/home-controller.js
@@ -4,10 +4,11 @@
     // how can we dynamically inject the resource, have hard coded organizations for now
     // need current model and then collection for list, eg collectionId 5 is model and courses is the collection etc
     myApp.controller( 'HomeCtrl',
-        function ( $scope , $rootScope, registryService, prefService , navService , fileService) {
+        function ( $scope , $rootScope, prefService , navService ) {
             $scope.go = function (where) {
                 navService.go ( 'views/main.html' ,{ collection : where ,
                     navDir: 'forward'});
             };
         } );
+        $scope.collection = prefService.getCourses();
 })(angular,_)// jshint ignore:line
